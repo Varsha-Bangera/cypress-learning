72023851194cdf13d8332a2b41f87d65e8f27ba0
varsha-bangera <varsha@qaoncloud.com>
2024-11-18 17:24:16 +0530

    commit

--- cypress/e2e/Script10 [Fileupload].cy.js
+++
@@ -1,7 +0,0 @@
-describe('File Upload Test',()=>{
-    it('file uplaod test',()=>{
-        cy.visit("https://fineuploader.com/demos.html")
-        const image = 'image.jpg';
-        cy.get('[name="qqfile"]').attachFile(image)
-    })
-})
--- cypress/e2e/Script11 [download file].cy.js
+++
@@ -1,3 +0,0 @@
-it("download image test",()=>{
-    cy.downloadFile('https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg','mydownloads','image.jpg')
-})
