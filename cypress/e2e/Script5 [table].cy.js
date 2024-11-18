a13b997e072dc0ca64f516341b3e63b49f8c889c
varsha-bangera <varsha@qaoncloud.com>
2024-11-18 21:50:51 +0530

    first commit

--- cypress/e2e/Script5 [table].cy.js
+++ cypress/e2e/Script5 [table].cy.js
@@ -10,7 +10,8 @@ describe('table',()=>{
         cy.wait(2000)
 
         //check value presence in specific row and column
-        cy.get('body > div:nth-child(4) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(3) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(3)').contains('Selenium').should('be.visible')
+        cy.get('body > div:nth-child(4) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(3) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(3)')
+        .contains('Selenium').should('be.visible')
         
         // verify book name using author amod
         cy.get('table[name=BookTable] tbody> tr td:nth-child(2)').each(($e1,index,$list)=> {
@@ -34,14 +35,14 @@ describe('table',()=>{
     it('second table',()=>{
         cy.visit("https://testautomationpractice.blogspot.com/")
 
-        cy.get("table[id='productTable'] tbody> tr td:nth-child(3)").each(($e,index,$list)=> {
+        cy.get('table[id=productTable] tbody> tr td:nth-child(3)').each(($e,index,$list)=> {
             const text=$e.text()
-            if(text.includes("$5.99"))
+            if(text.includes("$10.99"))
             {
                 cy.get("table[id='productTable'] tbody> tr td:nth-child(2)").eq(index).then(function(pname)
                 {
                     const productname=pname.text()
-                    expect(productname).to.equal("Product 3")
+                    expect(productname).to.equal("Smartphone")
                 })
             }
 
