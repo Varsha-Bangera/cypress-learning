fdbd899c8c56b5acc9b40e90bd6189cfdf00e647
varsha-bangera <varsha@qaoncloud.com>
2025-01-04 09:40:12 +0530

    changes

--- cypress/e2e/Script14 [drag and drop].cy.js
+++
@@ -1,9 +0,0 @@
-describe('Drag and drop test',()=>{
-
-    it('drag and drop',()=>{
-        
-        cy.visit('https://kitchen.applitools.com/ingredients/drag-and-drop')
-        cy.get('li[id="menu-fried-chicken"]').drag('ul[id="plate-items"] li')
-
-    })
-})
